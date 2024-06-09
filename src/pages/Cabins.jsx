1cf81c09f54ec53e75552e4dce34eb25e6c2eff1
kushagraag <kushjanhvi@gmail.com>
2024-06-09 14:44:22 +0530

    form in a modal

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-
 import CabinTables from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <>
       <Row type="horizontal">
@@ -17,10 +12,7 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTables />
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add new Cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
