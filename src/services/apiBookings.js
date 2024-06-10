ded816ed52af6f3397497acc2b94f47897f01a81
kushagraag <kushjanhvi@gmail.com>
2024-06-10 22:32:44 +0530

    filter in bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -16,12 +16,19 @@ export async function getBooking(id) {
   return data;
 }
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+
+  // filter
+  if (filter !== null) {
+    query = query.eq(filter.field, filter.value);
+  }
+
+  const { data, error } = await query;
 
   if (error) {
     console.log(error);
